2f8e0a07cd11830cd0fde89459efbddea0dc2505
Jason Kridner <jkridner@beagleboard.org>
2023-10-25 11:45:48 -0400

    CI: try removing typo3 theme

--- conf.py
+++ conf.py
@@ -43,7 +43,6 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_typo3_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
