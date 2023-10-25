64f031a86145dcd76697115cfa7b3e35eaf0f251
Jason Kridner <jkridner@beagleboard.org>
2023-10-25 11:46:55 -0400

    CI: try removing myst_parser

--- conf.py
+++ conf.py
@@ -47,7 +47,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
-    'myst_parser',
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.extlinks',
     'sphinx_rtd_theme',
@@ -70,7 +69,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
